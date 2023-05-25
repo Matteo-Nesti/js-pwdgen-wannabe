7cdf05a909be93ecee4ee91688e0d4d27f10685a
Matteo-Nesti <matteo.nesti@hotmail.com>
2023-05-25 15:51:33 +0200

    password generator js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,29 @@
 console.log('JS OKJS OK')
+
+// creare variabile stringa 21
+const sufixNumber = '21';
+console.log(sufixNumber);
+
+// chiedere info all'utente 
+
+const firstName = prompt('come ti chiami?');
+console.log(firstName);
+
+const lastName = prompt('qual\'e\' il tuo cognome?');
+console.log(lastName);
+
+const favouriteColor = prompt('e il tuo colore preferito?')
+console.log(favouriteColor);
+
+//concatenare le variabili
+
+const nomeX = firstName + lastName + favouriteColor;
+console.log(nomeX);
+
+const risultato = nomeX + sufixNumber;
+console.log(risultato);
+
+// selezionato elemento dal dom e risultato nell'elemento.
+
+document.getElementById('pass-gen').innerText = risultato;
+
